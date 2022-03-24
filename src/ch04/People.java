495736def6265a3bcd65db62579da4d3599ae47e
TAEJU <TAEJU@naver.com>
2022-03-24 15:08:06 +0900

    git commit test

--- src/ch04/People.java
+++ src/ch04/People.java
@@ -5,7 +5,7 @@ public class People {
 	// 다양성을 위해서 초기화 하지 않음
 	String name;
 	int age;
-	
+	// git commit test
 	// 디폴트 생성자 있을까? 있음 -> .java -> .class 컴파일할때 자바가 자동으로 코드 만들어줌
 	// 내가 직접 생성자를 정의하였기 때문에 디폴트 생성자 없음
 	public People(String name, int age) { // 인수 홍길동, 16을 넘겨서 매개변수 name, age가 받음
